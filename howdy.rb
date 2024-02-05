7ad7d825fb35e38bdcb330e134f7482f9fe8dc0d
Rebecca McKenzie <63881668+RMcKenzie111@users.noreply.github.com>
2024-02-05 23:48:55 +0000

    added howdy goodbye and gemfiles

---
+++ howdy.rb
@@ -0,0 +1,17 @@
+require "active_support/all"
+# some code
+#string = "Whubadubdub"
+#pp string
+
+pp 1.ordinalize
+pp 2.ordinalize
+
+pp "table".pluralize
+pp "ruby".pluralize
+
+pp "tables".singularize
+pp "rubies".singularize
+
+
+
+#require "./goodbye.rb"
