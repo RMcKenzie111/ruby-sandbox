d8dd531144a2330e022b00935eca1b4e528fb478
Rebecca McKenzie <63881668+RMcKenzie111@users.noreply.github.com>
2024-02-06 00:13:53 +0000

    getschomp and puts

--- howdy.rb
+++ howdy.rb
@@ -1,8 +1,17 @@
 require "active_support/all"
+
+pp "What's your name?"
+
+# chomp removes new line at the end of a string
+their_name = gets.chomp
+
+# puts has ! on a new line for some reason, chomp puts it one the same line
+puts "Hello, " + their_name + "!"
 # some code
 #string = "Whubadubdub"
 #pp string
 
+=begin
 pp 1.ordinalize
 pp 2.ordinalize
 
@@ -11,7 +20,7 @@ pp "ruby".pluralize
 
 pp "tables".singularize
 pp "rubies".singularize
-
+=end
 
 
 #require "./goodbye.rb"
